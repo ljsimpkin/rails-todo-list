19dd26f90324fef62946f86c63473d50541783db
Liam Simpkin <lsimpkin@comprehensivecare.co.nz>
2022-09-16 16:30:01 +1200

    add logout to main template

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # root 'todos#index'
-  root to: 'todos#index'
+  root 'todos#index'
   resources :todos
 end
